8b34bd19162013ce17b978b6f3243433e5741358
Tony <39646692+PrinceORS@users.noreply.github.com>
2023-07-01 09:45:16 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,9 +1,31 @@
 		let compChoice;
-		let playerChoice = ' ';
+		let playerChoice = '';
 		let turn = 0;
 		let playerWins = 0;
 		let compWins = 0;
 		let isGameOver = false;
+
+		function playGame(){
+			while(turn < 5){
+				if (isGameOver == true){break;}
+
+				console.log('Turn ' + (turn + 1));
+
+				console.log('You: ' + (playerChoice = getPlayerChoice()) + '  | Comp: ' + (compChoice = getComputerChoice()));
+
+				console.log(getTurnResult());
+
+				console.log('Player   ' + playerWins + '  | Computer ' + compWins);
+				console.log("");
+
+				checkGameStatus();
+			}
+		}
+
+		function getPlayerChoice(){
+			playerChoice = prompt('rock, paper or scissors');
+			return playerChoice.toLowerCase();
+		}
 
 		function getComputerChoice(){
 			compChoice = Math.floor(Math.random() * 3);
@@ -14,70 +36,52 @@
 				}else return 'scissors';
 		}
 
-		function playGame(){
-			while(turn < 5){
-				console.log('Turn ' + (turn + 1));
+		function getTurnResult(){
+			turn += 1;
 
-				if (isGameOver == true){break;}
-
-				playerChoice = prompt('rock, paper or scissors', '');
-
-				console.log('You: ' + (playerChoice.toLowerCase() + '  | Comp: ' + (compChoice = getComputerChoice())));
-
-				getTurnResult();
-
-				console.log('Player   ' + playerWins + '  | Computer ' + compWins);
-				console.log("");
-
-				// is the game is over (a score is over 3 or turns are finished)
-				if(playerWins > 2){
-					getGameResult();
-					isGameOver = true;
-					break;
-				}else if(compWins > 2){
-					getGameResult();
-					isGameOver = true;
-					break;
-				}else if(turn == 5){
-					getGameResult();
-					break;
-				}
+			if(compChoice == playerChoice){
+				return 'DRAW!';
+			}else if(compChoice == 'rock' && playerChoice == 'scissors'){
+				compWins += 1;
+				return 'YOU LOSE!';
+			}else if(compChoice == 'rock' && playerChoice == 'paper'){
+				playerWins += 1;
+				return 'YOU WIN!';
+			}else if(compChoice == 'paper' && playerChoice == 'scissors'){
+				playerWins += 1;
+				return 'YOU WIN!';
+			}else if(compChoice == 'paper' && playerChoice == 'rock'){
+				compWins += 1;
+				return 'YOU LOSE!';
+			}else if(compChoice == 'scissors' && playerChoice == 'rock'){
+				playerWins += 1;
+				return 'YOU WIN!';
+			}else if(compChoice == 'scissors' && playerChoice == 'paper'){
+				compWins += 1;
+				return 'YOU LOSE!';
 			}
 		}
 
-		function getTurnResult(){
-			if(compChoice == playerChoice){
-				console.log('DRAW!');
-			}else if(compChoice == 'rock' && playerChoice == 'scissors'){
-				console.log('YOU LOSE!');
-				compWins += 1;
-			}else if(compChoice == 'rock' && playerChoice == 'paper'){
-				console.log('YOU WIN!');
-				playerWins += 1;
-			}else if(compChoice == 'paper' && playerChoice == 'scissors'){
-				console.log('YOU WIN!');
-				playerWins += 1;
-			}else if(compChoice == 'paper' && playerChoice == 'rock'){
-				console.log('YOU LOSE!');
-				compWins += 1;
-			}else if(compChoice == 'scissors' && playerChoice == 'rock'){
-				console.log('YOU WIN!');
-				playerWins += 1;
-			}else if(compChoice == 'scissors' && playerChoice == 'paper'){
-				console.log('YOU LOSE!');
-				compWins += 1;
+		function checkGameStatus(){
+			// check if the game is over (a score is over 3 or turns are finished)
+			if(playerWins > 2){
+				console.log(getGameResult());
+				isGameOver = true;
+			}else if(compWins > 2){
+				console.log(getGameResult());
+				isGameOver = true;
+			}else if(turn == 5){
+				console.log(getGameResult());
 			}
-
-			turn += 1;
 		}
 
 		function getGameResult(){
 			if(playerWins > compWins){
-				console.log('*** YOU WIN ***');
+				return '* YOU WIN THE GAME *';
 			}else if(compWins > playerWins){
-				console.log('*** COMPUTER WINS ***');
+				return '* COMP WINS THE GAME *';
 			}else{
-				console.log('THE GAME IS A DRAW!');
+				return 'THE GAME IS A DRAW!';
 			}
 		}
 
--- readMe.txt
+++ readMe.txt
@@ -2,3 +2,5 @@ I became so engrossed in the project that I forgot to commit at the various stag
 
 Shortened the playGame function and included getTurnResult function
 Changed some function names to conform to best practice.
+
+Shortened the playGame function some more and included getGameStatus and getPlayerChoice functions.
